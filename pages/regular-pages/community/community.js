9593f7d1928325a52e89e2b415a1091243649fdf
yinz <yinzhong@goyoo.com>
2016-10-06 19:36:30 +0800

    社区 第二级, 接下来写tabs

--- pages/regular-pages/community/community.js
+++ pages/regular-pages/community/community.js
@@ -1,1 +1,44 @@
-Page({})
+var app = getApp()
+var {formatTime} = require('../../../utils/util.js')
+
+Page({
+    data: {
+        isLogin: false,
+        tabs: [],
+        userInfo: {},
+        list: []
+    },
+    onLoad() {
+        // 判断用户是否登录
+        if (app.globalData.userInfo) {
+            this.setData({
+                isLogin: true,
+                userInfo: app.globalData.userInfo
+            })
+        }
+        // 设置tabs
+        this.setData({
+            tabs: app.globalData.tabs
+        })
+
+        // 获取用户的主配置信息
+        app.api.forumList().then(res => {
+            const { list } = res
+            list.forEach(boardCategory => {
+                boardCategory.board_list.forEach(board => {
+                    board.url = `/pages/regular-pages/forum/forum?boardId=${board.board_id}&title=${board.board_name}`
+                    board.last_posts_date = formatTime(board.last_posts_date)
+                })
+            })
+            this.setData({ list })
+        })
+    },
+    // 改变全局的moduleId
+    changeModuleId(e) {
+        app.to(e.currentTarget.dataset.moduleId, true)
+    },
+    navigator(e) {
+        const { url } = e.currentTarget.dataset
+        wx.navigateTo({ url })
+    }
+})
---
+++ pages/regular-pages/forum/forum.js
@@ -0,0 +1,66 @@
+var app = getApp()
+var {formatTime} = require('../../../utils/util.js')
+
+Page({
+    data: {
+        isLogin: false,
+        userInfo: {},
+        title: {},
+        tabsIndex: 0,
+        forumInfo: {},
+        allList:[],
+        newList: [],
+        marrowList: []
+    },
+    onLoad(data) {
+        // 判断用户是否登录
+        if (app.globalData.userInfo) {
+            this.setData({
+                isLogin: true,
+                userInfo: app.globalData.userInfo
+            })
+        }
+        const { title, boardId } = data
+        this.setData({ title })
+
+        app.api.forum(boardId, { sortby: 'all' }).then(res => {
+            const { list: allList, forumInfo, total_num, topTopicList } = res
+            this.setData({
+                allList: formateList(allList),
+                forumInfo,
+                topTopicList,
+                totalNum: total_num
+            })
+        })
+        app.api.forum(boardId, { sortby: 'new' }).then(res => {
+            const { list: newList } = res
+            this.setData({ newList: formateList(newList) })
+        })
+        app.api.forum(boardId, { sortby: 'marrow' }).then(res => {
+            const { list: marrowList } = res
+            this.setData({ marrowList: formateList(marrowList) })
+        })
+    },
+    onReady() {
+        const { title } = this.data
+        wx.setNavigationBarTitle({ title })
+    },
+    changeTabs(e) {
+        const { index: tabsIndex } = e.currentTarget.dataset
+        this.setData({ tabsIndex })
+    }
+})
+
+
+function formateList (list) {
+    list.forEach(item => {
+        item.imageList = item.imageList.map(src =>
+            src.replace('xgsize_', 'mobcentSmallPreview_')
+        )
+        if (item.imageList.length > 3) {
+            item.imageList.length = 3
+        }
+        item.last_reply_date = formatTime(item.last_reply_date)
+    })
+    return list
+}
