04173d2e55b43094d26165c1bdea448e54e5d7d8
coolfish <461836324@qq.com>
2016-10-08 10:58:22 +0800

    处理格式化

--- pages/regular-pages/topic-list/topic-list.js
+++ pages/regular-pages/topic-list/topic-list.js
@@ -1,54 +1,62 @@
 var app = getApp()
 
 Page({
-  data: {
-    isLogin: false,
-    tabs:[],
-    userInfo: {},
-    topic: {},
-    mytopic: {},
-    color: '#000'
-  },
-  // 改变全局的moduleId
-  changeModuleId: function (e) {
-    app.to(e.currentTarget.dataset.moduleId, true)
-  },
-  featchData: function () {
-    var querys = [ app.api.topic() ];
-    if (this.data.isLogin) {
-      querys.push(app.api.mytopic())
-    }
-    Promise
-      .all(querys)
-      .then(result => {
-        console.log(result)
+    data: {
+        isLogin: false,
+        tabs:[],
+        userInfo: {},
+        topic: {},
+        mytopic: {},
+        color: '#000'
+    },
+    // 改变全局的moduleId
+    changeModuleId: function (e) {
+        app.to(e.currentTarget.dataset.moduleId, true)
+    },
+    featchData: function () {
+        var querys = [ app.api.topic() ];
+        if (this.data.isLogin) {
+            querys.push(app.api.mytopic())
+        }
+        Promise
+            .all(querys)
+            .then(result => {
+                console.log(result)
+                this.setData({
+                    topic: result[0],
+                    mytopic: (result.length > 1 ? result[1] : {})
+                })
+            })
+    },
+    onLoad: function () {
+        console.log(app.globalData)
+        // 设置tabs 和 color
         this.setData({
-          topic: result[0],
-          mytopic: (result.length > 1 ? result[1] : {})
+            tabs: app.globalData.tabs,
+            color: app.globalData.info.appColor
         })
-      })
-  },
-  onLoad: function () {
-    // 设置tabs 和 color
-    this.setData({
-      tabs: app.globalData.tabs,
-      color: app.globalData.info.appColor
-    })
-    // 判断用户是否登录
-    if (app.globalData.userInfo) {
-      this.setData({
-        isLogin: true,
-        userInfo: app.globalData.userInfo
-      })
+        // 判断用户是否登录
+        if (app.globalData.userInfo) {
+            this.setData({
+                isLogin: true,
+                userInfo: app.globalData.userInfo
+            })
+        }
+        // 获取数据
+        this.featchData()
+    },
+    onReady: function () {
+        var title = app.globalData.modules[app.globalData.moduleId].componentList[0].title
+        // 修改导航条
+        wx.setNavigationBarTitle({
+            title: title
+        })
+    },
+    toLogin: function () {
+        if (!this.data.isLogin){
+            wx.navigateTo({
+                url:'/pages/regular-pages/login/login'
+            })
+        }
     }
-    // 获取数据
-    this.featchData()
-  },
-  onReady: function () {
-    var title = app.globalData.modules[app.globalData.moduleId].componentList[0].title
-    // 修改导航条
-    wx.setNavigationBarTitle({
-      title: title
-    })
-  }
 })
--- pages/regular-pages/topic/topic.js
+++ pages/regular-pages/topic/topic.js
@@ -1,79 +1,79 @@
 var app = getApp(),
-  util = require('../../../utils/util.js')
+    util = require('../../../utils/util.js')
 
 Page({
-  data: {
-    id: '',
-    tpcinfo: {},
-    topUser: [],
-    partinNum: 0,
-    newList: [],
-    newPage: 0,
-    newTotalNum: 0,
-    hotList: [],
-    hotPage: 0,
-    hotTotalNum: 0,
-    color: '#000',
-    currentIndex: 0,
-    tabs: ['最新', '最热']
-  },
-  onLoad: function (data) {
-    console.log(app)
-    this.setData({
-      id: data.id,
-      color: app.globalData.info.appColor
-    })
-    this.featchData()
-  },
-  featchData: function () {
-    // NEW HOT
-    var ti_id = this.data.id
-    Promise
-      .all([
-        app.api.topicdtl({ 'ti_id': ti_id, 'orderby': 'NEW' }), 
-        app.api.topicdtl({ 'ti_id': ti_id, 'orderby': 'HOT' })
-      ])
-      .then(([ resultByNew, resultByHot ]) => {
-        resultByNew.list = resultByNew.list.map(v => {
-          v.imageList = v.imageList.map(src => src.replace('xgsize_', 'mobcentSmallPreview_'))
-          v.last_reply_date = util.formatTime(v.last_reply_date)
-          v.subject = util.formateText(v.subject)
-          v.title = util.formateText(v.title)
-          return v
-        })
-        resultByHot.list = resultByHot.list.map(v => {
-          v.imageList = v.imageList.map(src => src.replace('xgsize_', 'mobcentSmallPreview_'))
-          v.last_reply_date = util.formatTime(v.last_reply_date)
-          v.subject = util.formateText(v.subject)
-          v.title = util.formateText(v.title)
-          return v
-        })
+    data: {
+        id: '',
+        tpcinfo: {},
+        topUser: [],
+        partinNum: 0,
+        newList: [],
+        newPage: 0,
+        newTotalNum: 0,
+        hotList: [],
+        hotPage: 0,
+        hotTotalNum: 0,
+        color: '#000',
+        currentIndex: 0,
+        tabs: ['最新', '最热']
+    },
+    onLoad: function (data) {
+        console.log(app)
         this.setData({
-          tpcinfo: resultByNew.tpcinfo,
-          topUser: resultByNew.topUser,
-          partinNum: resultByNew.partinNum,
-          newList: resultByNew.list,
-          newPage: resultByNew.page,
-          newTotalNum: resultByNew.total_num,
-          hotList: resultByHot.list,
-          hotPage: resultByHot.page,
-          hotTotalNum: resultByHot.total_num,
+            id: data.id,
+            color: app.globalData.info.appColor
         })
-      })
-  },
-  onReady: function () {
-    console.log(this.data)
-    var title = this.data.tpcinfo.ti_title
-    // 修改导航条
-    wx.setNavigationBarTitle({
-      title: title
-    })
-  },
-  changeTabs: function (e) {
-    // 切换选项卡
-    this.setData({
-      currentIndex: e.currentTarget.dataset.index
-    })
-  }
+        this.featchData()
+    },
+    featchData: function () {
+        // NEW HOT
+        var ti_id = this.data.id
+        Promise
+            .all([
+                app.api.topicdtl({ 'ti_id': ti_id, 'orderby': 'NEW' }), 
+                app.api.topicdtl({ 'ti_id': ti_id, 'orderby': 'HOT' })
+            ])
+            .then(([ resultByNew, resultByHot ]) => {
+                resultByNew.list = resultByNew.list.map(v => {
+                    v.imageList = v.imageList.map(src => src.replace('xgsize_', 'mobcentSmallPreview_'))
+                    v.last_reply_date = util.formatTime(v.last_reply_date)
+                    v.subject = util.formateText(v.subject)
+                    v.title = util.formateText(v.title)
+                    return v
+                })
+                resultByHot.list = resultByHot.list.map(v => {
+                    v.imageList = v.imageList.map(src => src.replace('xgsize_', 'mobcentSmallPreview_'))
+                    v.last_reply_date = util.formatTime(v.last_reply_date)
+                    v.subject = util.formateText(v.subject)
+                    v.title = util.formateText(v.title)
+                    return v
+                })
+                this.setData({
+                    tpcinfo: resultByNew.tpcinfo,
+                    topUser: resultByNew.topUser,
+                    partinNum: resultByNew.partinNum,
+                    newList: resultByNew.list,
+                    newPage: resultByNew.page,
+                    newTotalNum: resultByNew.total_num,
+                    hotList: resultByHot.list,
+                    hotPage: resultByHot.page,
+                    hotTotalNum: resultByHot.total_num,
+                })
+            })
+    },
+    onReady: function () {
+        console.log(this.data)
+        var title = this.data.tpcinfo.ti_title
+        // 修改导航条
+        wx.setNavigationBarTitle({
+            title: title
+        })
+    },
+    changeTabs: function (e) {
+        // 切换选项卡
+        this.setData({
+            currentIndex: e.currentTarget.dataset.index
+        })
+    }
 })
 
