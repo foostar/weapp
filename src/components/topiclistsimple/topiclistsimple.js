031562243da44011968f8ef39277062a71eb7f9b
qiushi <qiushi@qiushideMacBook-Pro.local>
2016-10-20 21:07:23 +0800

    滚动加载

--- src/components/topiclistsimple/topiclistsimple.js
+++ src/components/topiclistsimple/topiclistsimple.js
@@ -2,25 +2,18 @@ const Component = require('../../lib/component')
 const util = require('../../utils/util')
 
 const app = getApp()
-
+var i = 1
 function TopiclistSimple(key, module) {
     Component.call(this, key)
-    app.api.forum(module.extParams.forumId, {
-        sortby: module.extParams.orderby || 'all'
-    }).then((data) => {
-        data.list = data.list.map((v) => {
-            v.imageList = v.imageList.map(src => src.replace('xgsize_', 'mobcentSmallPreview_'))
-            v.last_reply_date = util.formatTime(v.last_reply_date)
-            v.subject = util.formateText(v.subject)
-            return v
-        })
-        this.setData({
-            module,
-            resources: data,
-            isLoading: true,
-            appIcon: app.globalData.info.appIcon
-        })
-    })
+    // 添加分页
+    this.data = {
+        page:1,
+        module,
+        resources: {},
+        isLoading: false,
+        appIcon: app.globalData.info.appIcon,
+        endPage: 0
+    }
 }
 
 TopiclistSimple.prototype = Object.create(Component.prototype)
@@ -29,4 +22,43 @@ TopiclistSimple.prototype.constructor = TopiclistSimple
 TopiclistSimple.prototype.clickItem = function (e) {
     app.showPost(e.currentTarget.id)
 }
+
+TopiclistSimple.prototype.onLoad = function(){
+    this.fetchData(this.data.module, this.data.page)
+    let that = this
+    // 加载下一页的数据
+    app.event.on('nextPage', function(){
+        let { page, endPage } = that.data
+        if (page < endPage) {
+            that.fetchData(that.data.module, page + 1)
+        }
+    })
+}
+// 请求数据
+TopiclistSimple.prototype.fetchData = function (module, page){
+    let list = this.data.resources.list ? this.data.resources.list : []
+    app.api.forum(module.extParams.forumId, {
+        page,
+        sortby: module.extParams.orderby || 'all'
+    }).then((data) => {
+        data.list = data.list.map((v) => {
+            v.imageList = v.imageList.map(src => src.replace('xgsize_', 'mobcentSmallPreview_'))
+            v.last_reply_date = util.formatTime(v.last_reply_date)
+            v.subject = util.formateText(v.subject)
+            return v
+        })
+        data.list = list.concat(data.list)
+        console.log(data)
+        this.setData({
+            module,
+            page,
+            resources: data,
+            isLoading: true,
+            endPage: parseInt(data.total_num / 20 + 1)
+        })
+    })
+}
+
+
+
 module.exports = TopiclistSimple
--- src/utils/util.js
+++ src/utils/util.js
@@ -9,6 +9,49 @@ function formateText(str = '', length = 40) {
     return str
 }
 
+/**
+ * 无限加载
+ *
+ * 具体描述一些细节
+ *
+ * @param    {string}  url     地址
+ * @param    {string}  url     地址
+
+
+ * @param    {array}   com         商品数组
+ * @param    {string}  pay_status  支付方式
+ * @returns  void
+ *
+ * @date     2014-04-12
+ * @author   QETHAN<qinbinyang@zuijiao.net>
+ */
+
+function requestFun(url, options, callback){
+    let method = 'GET'
+    let header = {
+        'Content-Type': 'application/json'
+    }
+    let data = { }
+    if (typeof options !== 'function') {
+        method = options.method ? options.method: 'GET'
+        header = options.header ? options.header: header
+        data = options.data ? options.data: data
+    }
+    wx.request({
+        url,
+        method,
+        header,
+        data,
+        success: function(res) {
+            return callback(null, res)
+        },
+        fail:function(err) {
+            return callback(err)            
+        }
+    })
+}
+
+
 const dateFormat = (date, format = 'yyyy-MM-dd hh:mm:ss', readability = true) => {
     if (!date) return ''
     if (typeof date === 'string' && /^\d+$/.test(date)) {
