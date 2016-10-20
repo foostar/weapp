e0e638828fd9adf67065f30bf47bf1f19efff85c
tengfei <shitengfei@goyoo.com>
2016-10-20 10:53:26 +0800

    修改subnav样式

--- src/components/subnavflat/subnavflat.js
+++ src/components/subnavflat/subnavflat.js
@@ -11,15 +11,19 @@ function SubnavFlat(key, module) {
         modules[x.id] = x
     })
 
+    const tabs = module.componentList.map((x) => {
+        return {
+            id: x.id,
+            title: x.title
+        }
+    })
+
     this.data = {
+        index: 0,
+        width: `${100 / tabs.length}%`,
         selected: module.componentList[0].id,
         modules,
-        tabs: module.componentList.map((x) => {
-            return {
-                id: x.id,
-                title: x.title
-            }
-        })
+        tabs
     }
 }
 
@@ -29,7 +33,9 @@ SubnavFlat.prototype.constructor = SubnavFlat
 
 SubnavFlat.prototype.switchTab = function (event) {
     const id = event.currentTarget.dataset.id
+    const index = event.currentTarget.dataset.index
     this.setData({
+        index,
         selected: id
     })
     this.children[`m_${id}`].load()
