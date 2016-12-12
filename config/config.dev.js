12caaedb0dd3426321858c9dd69c851336a96026
xiusiteng <xiusiteng@goyoo.com>
2016-12-12 16:50:14 +0800

    fixed

---
+++ config/config.dev.js
@@ -0,0 +1,11 @@
+module.exports = {
+    appList: [ {
+        name: '测试', id: 100000000187,
+    }, {
+        name: 'life', id: 100000000245,
+    } ],
+    config: {
+        cmsUrl: 'http://test-cmsapi.app.xiaoyun.com/GpCmsApi',
+        HOST: 'https://weapp-test.v2.yoo.yunpro.cn'
+    }
+}
