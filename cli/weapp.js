283c02aa07bd77201fdcbce3e5e18d4f994487c3
xiusiteng <xiusiteng@goyoo.com>
2016-12-12 16:23:27 +0800

    fixed

--- cli/weapp.js
+++ cli/weapp.js
@@ -1,13 +1,12 @@
 #!/usr/bin/env node
 const program = require('commander')
 const selectShell = require('select-shell')
-const { generateConfigByAppId, writeFile } = require('../lib/app')
-const path = require('path')
+const { generateConfigByAppId } = require('../lib/app')
 require('colors')
 
-const genConfig = (appId) => {
+const genConfig = (appId, config) => {
     console.log(`正在获取 app: <${appId}> 数据...`)
-    generateConfigByAppId(appId).then(() => {
+    generateConfigByAppId(appId, {}, config).then(() => {
         console.log(`生成 <${appId}> 配置完成`.green)
         process.exit(0)
     }, (err) => {
@@ -25,8 +24,15 @@ program
     .command('init [appId]')
     .description('初始化小程序配置')
     .action((appId) => {
+        /* eslint-disable */
+        let env = require('../config/config.pro.js')
+        if (program.env) {
+            env = require('../config/config.dev.js')
+        }
+        /* eslint-enable */
+        const { config, appList } = env
         if (appId) {
-            return genConfig(appId)
+            genConfig(appId, config)
         }
         const list = selectShell({
             pointer: ' ▸ ',
@@ -40,48 +46,13 @@ program
             inverse: false,
             prepend: false
         })
-
-        list
-            .option('蒙城汇', 127597)
-            .option('好友驿站', 231345)
-            .option('爱钓鱼', 177432)
-            .option('龟友天下', 55911)
-            .option('郑州在线', 176489)
-            .option('淋巴瘤之家', 18894)
-            .option('乐清上班族网', 113825)
-            .option('重钓网', 47269)
-            .option('龙岩KK网', 73245)
-            .option('四平英城网', 118151)
-            .option('----- 以上为预售购买用户，需要特殊照顾 -----', null)
-            .option('测试31G', 210380)
-            .option('新青年麻醉', 85576)
-            .option('编织人生', 70909)
-            .option('吴川脚爆爆', 98984)
-            .option('丹东妈妈网', 127539)
-            .option('穿针引线', 73551)
-            .option('湖南妈妈网', 73529)
-            .option('智慧登封', 124307)
-            .option('小云社群', 229171)
-            .option('测试', 100000000187)
-            .option('life', 100000000245)
-            .list()
-
+        appList.forEach((v) => {
+            list.option(v.name, v.id)
+        })
+        list.list()
         list.on('select', options => {
-            let config = {
-                cmsUrl: 'http://cmsapi.app.xiaoyun.com/GpCmsApi',
-                HOST: 'https://weapp.apps.xiaoyun.com'
-            }
-            if (program.env) {
-                config = {
-                    cmsUrl: 'http://test-cmsapi.app.xiaoyun.com/GpCmsApi',
-                    HOST: 'https://weapp-test.v2.yoo.yunpro.cn'
-                }
-            }
             if (options[0].value) {
-                writeFile(path.join(__dirname, '../lib/env.js'), `/* eslint-disable */\nmodule.exports=${JSON.stringify(config)}`)
-                .then(() => {
-                    genConfig(options[0].value)
-                })
+                genConfig(options[0].value, config)
             } else {
                 process.exit(0)
             }
