444b7c2ddb4ae96e3165c5d32b608fb392e55e55
xiusiteng <xiusiteng@goyoo.com>
2016-12-15 20:01:00 +0800

    0.1.9

--- buildtest.sh
+++ buildtest.sh
@@ -1,2 +1,2 @@
-docker build -t dhub.yunpro.cn/huston/weapptest:0.1.7 .
-docker push dhub.yunpro.cn/huston/weapptest:0.1.7
+docker build -t dhub.yunpro.cn/huston/weapptest:0.1.8 .
+docker push dhub.yunpro.cn/huston/weapptest:0.1.8
