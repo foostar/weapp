af5d72fb216aa5fdad7b5031127df6b38af9146e
xiusiteng <xiusiteng@goyoo.com>
2017-01-09 18:03:29 +0800

    fixed

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-docker build -t dhub.yunpro.cn/tengfei/weapp-d:0.2.2 .
-docker push dhub.yunpro.cn/tengfei/weapp-d:0.2.2
+docker build -t dhub.yunpro.cn/tengfei/weapp-d:0.2.1 .
+docker push dhub.yunpro.cn/tengfei/weapp-d:0.2.1
