9d8535d6ff2306bebcc88ddb91fc9b56e6fe364e
xiusiteng <xiusiteng@goyoo.com>
2017-02-07 13:48:38 +0800

    fixed

--- buildtest.sh
+++ buildtest.sh
@@ -1,2 +1,2 @@
-docker build -t dhub.yunpro.cn/huston/weapptest:0.2.35 .
-docker push dhub.yunpro.cn/huston/weapptest:0.2.35
+docker build -t dhub.yunpro.cn/huston/weapptest:0.2.4 .
+docker push dhub.yunpro.cn/huston/weapptest:0.2.4
