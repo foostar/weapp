ab1bc493eca57ee9f8e9696319879763581ee3b7
xiusiteng <xiusiteng@goyoo.com>
2017-02-08 13:49:36 +0800

    fixed

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-docker build -t dhub.yunpro.cn/tengfei/weapp-d:0.2.35 .
-docker push dhub.yunpro.cn/tengfei/weapp-d:0.2.35
+docker build -t dhub.yunpro.cn/tengfei/weapp-d:0.2.4 .
+docker push dhub.yunpro.cn/tengfei/weapp-d:0.2.4
--- buildtest.sh
+++ buildtest.sh
@@ -1,2 +1,2 @@
-docker build -t dhub.yunpro.cn/huston/weapptest:0.2.4 .
-docker push dhub.yunpro.cn/huston/weapptest:0.2.4
+docker build -t dhub.yunpro.cn/huston/weapptest:0.2.41 .
+docker push dhub.yunpro.cn/huston/weapptest:0.2.41
